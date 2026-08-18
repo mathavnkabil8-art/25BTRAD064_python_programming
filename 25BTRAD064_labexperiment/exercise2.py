a94128e7538b2720806416086f05cf8b00cb932a
mathavnkabil8-art <mathavnkabil8@gmail.com>
2026-08-18 22:09:20 +0530

    Create exercise2.py

---
+++ 25BTRAD064_labexperiment/exercise2.py
@@ -0,0 +1,6 @@
+# 1. INPUT: Get the temperature in Celsius
+celsius = float(input("Enter temperature in Celsius: "))
+# Formula: (Celsius * 9/5) + 32
+fahrenheit = (celsius * 1.8) + 32
+# 2. OUTPUT: Display the result
+print("Temperature in Fahrenheit:", fahrenheit)
